a23fceb78a69c2869b7c5a836b36506323c4de5c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-02-29 11:06:21 -0500

    Add comments

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 
+//GET request for home page
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 const port = 3000;
+//Create server at port listed in port variable
 app.listen(port, () => {
   console.log(`port is listening on port ${port}`);
 });
