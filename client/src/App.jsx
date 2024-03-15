0f238ccff12879ab21d63879af59ae4e9e5d2e35
Matt DeMartino <matt.s.demartino@gmail.com>
2024-03-15 16:19:40 -0400

    Add colors theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 // Wrapper for Mantine Library
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
 // Required CSS for mantine
 import "@mantine/core/styles.css";
 //Wrappers for React Router
@@ -8,10 +8,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NavFooter from "./pages/NavFooter";
 
+// Array of colors for custom theme
+const myColors = [
+  "#e5f9ff",
+  "#d4eefa",
+  "#aad9f0",
+  "#7dc4e7",
+  "#59b2df",
+  "#42a7db",
+  "#32a1da",
+  "#218dc2",
+  "#0f7dae",
+  "#006c9b",
+];
+
+// Create theme using myColors array
+const theme = createTheme({ colors: { myColors } });
+
 function App() {
   return (
     <BrowserRouter>
-      <MantineProvider>
+      <MantineProvider theme={theme}>
         <Routes>
           {/*NavBar wrapper */}
           <Route path="/" element={<NavFooter />}>
