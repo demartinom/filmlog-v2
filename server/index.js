285b3a423ff9e98805b9de0d1bd11ba7113b29f7
Matt DeMartino <matt.s.demartino@gmail.com>
2024-03-22 15:41:12 -0400

    Move port to env and add formats router

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import process from "process";
 
 const app = express();
 // Router imports
 import films from "./routes/films.js";
 import makers from "./routes/makers.js";
 import types from "./routes/types.js";
+import formats from "./routes/formats.js";
 
 //GET request for home page
 app.get("/", (req, res) => {
@@ -15,8 +17,9 @@ app.get("/", (req, res) => {
 app.use("/api/films", films);
 app.use("/api/makers", makers);
 app.use("/api/types", types);
+app.use("api/formats", formats);
 
-const port = 3000;
+const port = process.env.PORT;
 //Create server at port listed in port variable
 app.listen(port, () => {
   console.log(`port is listening on port ${port}`);
