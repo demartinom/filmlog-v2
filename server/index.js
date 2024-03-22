fdf7ab94e8f6713e0fa0223cdbbd37a68033229d
Matt DeMartino <matt.s.demartino@gmail.com>
2024-03-22 12:55:49 -0400

    Import films router

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 
 const app = express();
+// Films routes
+import films from "./routes/films.js";
 
 //GET request for home page
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+// Films router
+app.use("/api/films", films);
 
 const port = 3000;
 //Create server at port listed in port variable
