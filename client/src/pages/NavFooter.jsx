93f02dbe6b4857dc6e9f77229b6e9e52413ae124
Matt DeMartino <matt.s.demartino@gmail.com>
2024-03-23 12:40:45 -0400

    Create prelim nav bar

--- client/src/pages/NavFooter.jsx
+++ client/src/pages/NavFooter.jsx
@@ -1,12 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import { Group, Text } from "@mantine/core";
 
 export default function NavFooter() {
   return (
     <>
-      <header>Nav</header>
+      <header>
+        <Group justify="space-between" px={20} py={10} bg="myColors.3">
+          <Text size="2rem">
+            <Link to="/">FilmLog</Link>
+          </Text>
+          <Group>
+            <Text size="2rem">
+              <Link to="/explore">Explore</Link>
+            </Text>
+            <Text size="2rem">
+              <Link to="/log">My Log</Link>
+            </Text>
+          </Group>
+          <Text size="2rem">
+            <Link to="/login">Login</Link>
+          </Text>
+        </Group>
+      </header>
       {/*Allows NavBar and Footer to render on all pages */}
       <Outlet />
-      <footer>Footer</footer>
+      <footer>
+        <Text px={20} py={10} bg="myColors.3">
+          Made by Matt DeMartino
+        </Text>
+      </footer>
     </>
   );
 }
