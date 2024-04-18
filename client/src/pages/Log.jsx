5b1cb9c89bd142d62b5eff6a6b6555e82c5a5f5b
Matt DeMartino <matt.s.demartino@gmail.com>
2024-04-18 12:29:23 -0400

    Format date for display in edit modal

--- client/src/pages/Log.jsx
+++ client/src/pages/Log.jsx
@@ -183,3 +183,12 @@ function formatDate(rollDate) {
   }/${dateStarted.getDate()}/${dateStarted.getFullYear().toString().slice(-2)}`;
   return formattedDateStarted;
 }
+
+function formatDateData(dateString) {
+  const dateObject = new Date(dateString);
+  const year = dateObject.getUTCFullYear(); // Using UTC methods
+  const month = String(dateObject.getUTCMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(dateObject.getUTCDate()).padStart(2, "0");
+
+  return new Date(`${year},${month},${day}`);
+}
