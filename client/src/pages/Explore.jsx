c05d264fda7b8b247bfefad59aed9e572d377b03
Matt DeMartino <matt.s.demartino@gmail.com>
2024-04-18 13:25:45 -0400

    Create api call to get all film stock data and set to state

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,4 +1,6 @@
 import { AppShellMain } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 /*TODO:
     API call for all film stocks
@@ -8,5 +10,36 @@ import { AppShellMain } from "@mantine/core";
 */
 
 export default function Explore() {
-  return <AppShellMain></AppShellMain>;
+  // State for information on all film stocks
+  const [filmStockData, setFilmStockData] = useState([]);
+  // Loading state while receiving data
+  const [loading, setLoading] = useState(true);
+
+  // api call to get all film stock data
+  useEffect(() => {
+    async function getStocks() {
+      try {
+        const res = await axios.get("/api/films/all");
+        setFilmStockData(res.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+      //Set loading to false to display data
+    }
+    getStocks();
+  }, []);
+
+  // Map over all film stocks to display information
+  const filmData = filmStockData.map((stock) => (
+    <div key={stock.id}>{stock.name}</div>
+  ));
+
+  // Renders loading when fetching data
+  if (loading) {
+    return <AppShellMain>Loading...</AppShellMain>;
+  }
+  // Render returned data
+  return <AppShellMain>{filmData}</AppShellMain>;
 }
