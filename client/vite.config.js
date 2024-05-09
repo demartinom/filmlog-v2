631ff9d09c4b2259c57adc13c6c3d4908d872be3
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-09 15:25:47 -0400

    Remove uneeded env config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": "http://localhost:3000",
-//     },
-//   },
-// });
-
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  const envVars = loadEnv(mode, process.cwd());
-  return defineConfig({
-    // To a plugins: [react()],
-    server: {
-      proxy: {
-        "/api": envVars.VITE_API_URL,
-      },
+//https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
     },
-  });
-};
+  },
+});
