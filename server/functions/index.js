4d6051504b6332bdbc2e1ffa00af04e613021a69
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-09 15:35:07 -0400

    Implement cors

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,10 +2,22 @@ const functions = require("firebase-functions");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
 const app = express();
+
+const corsOptions = {
+  origin: [
+    "https://filmlog.vercel.app",
+    "https://filmlog-v2-demartinoms-projects.vercel.app/",
+    "https://filmlog-v2-git-main-demartinoms-projects.vercel.app/",
+    "https://filmlog-v2-euxcz82yf-demartinoms-projects.vercel.app/",
+  ],
+};
 
 // Allows express to parse JSON request bodies
 app.use(bodyParser.json());
+// Allows for cors headers
+app.use(cors(corsOptions));
 
 // Router imports
 const filmsRouter = require("../routes/films");
