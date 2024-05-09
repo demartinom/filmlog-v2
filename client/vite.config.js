ef705c2698c7542fb2fb9ac347486559d3fbd958
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-09 13:12:22 -0400

    Add api url to env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": "http://localhost:3000",
+//     },
+//   },
+// });
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  const envVars = loadEnv(mode, process.cwd());
+  return defineConfig({
+    // To a plugins: [react()],
+    server: {
+      proxy: {
+        "/api": envVars.VITE_API_URL,
+      },
     },
-  },
-});
+  });
+};
