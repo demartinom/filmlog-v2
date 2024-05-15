3f4c47833ab0ed43fae6d08186b4da1704e36ed0
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-15 11:12:17 -0400

    Update redirect page

--- client/src/pages/Redirect.jsx
+++ client/src/pages/Redirect.jsx
@@ -3,12 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 // When user logs in or logs out, they are redirected to this page by SupaBase. This page redirects them to the proper home page URL
 export default function Redirect() {
-  let nav = useNavigate();
-  async function redirect() {
-    nav("/");
-    window.location.reload();
-  }
+  const navigate = useNavigate();
+
   useEffect(() => {
-    redirect();
-  }, []);
+    navigate("/");
+  }, [navigate]);
+
+  return null; // Since this component only handles redirection, it doesn't render anything
 }
