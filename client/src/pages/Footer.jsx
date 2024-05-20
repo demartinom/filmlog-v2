8db904fb0e5521ab2d9ea8d7d2722f681dbf8da1
Matt DeMartino <matt.s.demartino@gmail.com>
2024-05-20 11:31:46 -0400

    Add link to portfolio in footer

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { AppShellFooter, Text, Anchor, Group } from "@mantine/core";
+import { AppShellFooter, Anchor, Group } from "@mantine/core";
 
 export default function Footer() {
   return (
-    <AppShellFooter bg="myColors.3" pos={'absolute'} left={0} right={0} bottom={0}>
-      <Group py={5}>
-        <Text px={20} bg="myColors.3">
+    <AppShellFooter
+      bg="myColors.3"
+      pos={"absolute"}
+      left={0}
+      right={0}
+      bottom={0}
+    >
+      <Group py={5} px={20}>
+        <Anchor
+          underline="never"
+          c={"black"}
+          href="http://www.mattdemartino.com"
+        >
           Made by Matt DeMartino
-        </Text>
+        </Anchor>
         <Anchor underline="never" c={"black"} href="https://storyset.com/love">
           Home Page Illustration by Storyset
         </Anchor>
