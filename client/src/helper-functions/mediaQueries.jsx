4ecbf564a5947664a80619813030b972eedc173b
Matt DeMartino <matt.s.demartino@gmail.com>
2024-06-21 15:53:43 -0400

    Update small media query

--- client/src/helper-functions/mediaQueries.jsx
+++ client/src/helper-functions/mediaQueries.jsx
@@ -1,7 +1,7 @@
 import { useMediaQuery } from "@mantine/hooks";
 
 export const useResponsive = () => {
-  const isSmall = useMediaQuery("(min-width: 370px) and (max-width: 767px)");
+  const isSmall = useMediaQuery("(min-width: 365px) and (max-width: 767px)");
   const isMedium = useMediaQuery("(min-width: 768px) and (max-width: 1023px)");
   const isLarge = useMediaQuery("(min-width: 1024px) and (max-width: 1399px)");
   const isExtraLarge = useMediaQuery("(min-width: 1400px)");
