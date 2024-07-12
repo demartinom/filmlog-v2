a24544b4de9c999836c40e81f63e728ae9cce290
Matt DeMartino <matt.s.demartino@gmail.com>
2024-07-12 15:26:08 -0400

    Add route to get user stats

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -51,4 +51,55 @@ router.get("/all", async (req, res) => {
   }
 });
 
+router.get("/:user", async (req, res) => {
+  let { user } = req.params;
+  try {
+    const [totalRolls, mostPopularFormat, mostPopularStock, mostPopularType] =
+      await Promise.all([
+        prisma.userCount.aggregate({
+          _sum: {
+            count: true,
+          },
+          where: { userId: { contains: user } },
+        }),
+        prisma.userCount.groupBy({
+          by: ["format"],
+          where: { userId: { contains: user } },
+          _sum: { count: true },
+          orderBy: { _sum: { count: "desc" } },
+          take: 1,
+        }),
+        prisma.userCount.groupBy({
+          by: ["filmStockId"],
+          where: { userId: { contains: user } },
+          _sum: { count: true },
+          orderBy: { _sum: { count: "desc" } },
+          take: 1,
+        }),
+        prisma.userCount.groupBy({
+          by: ["type"],
+          where: { userId: { contains: user } },
+          _sum: { count: true },
+          orderBy: { _sum: { count: "desc" } },
+          take: 1,
+        }),
+      ]);
+    let mostPopularStockName = "";
+    if (mostPopularStock.length > 0) {
+      mostPopularStockName = await prisma.filmStock.findUnique({
+        where: { id: mostPopularStock[0].filmStockId },
+      });
+    }
+    res.json({
+      userTotalRolls: totalRolls._sum.count,
+      userMostPopularFormat: mostPopularFormat[0].format,
+      userMostPopularStock: mostPopularStockName.name,
+      userMostPopularType: mostPopularType[0].type,
+    });
+  } catch (error) {
+    console.log("Error fetching user stats", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 export default router;
