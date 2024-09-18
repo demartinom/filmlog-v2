88a7b363e727187bf78c60b8f645bf9501b0fd3c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-09-18 11:24:57 -0400

    Begin stats page

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,8 +1,35 @@
-import { AppShellMain } from '@mantine/core'
-import React from 'react'
+import { AppShellMain, Center, Title, Text, Stack, Group } from "@mantine/core";
+import React from "react";
 
-export default function Stats() {
+export default function Stats({ allRolls, user }) {
   return (
-    <AppShellMain></AppShellMain>
-  )
+    <AppShellMain>
+      <Center>
+        <Title>Roll Statistics</Title>
+      </Center>
+      <Center>
+        <Stack ta={"center"}>
+          <Title>Total Rolls logged in FilmLog: {allRolls.totalRolls}</Title>
+          <Group>
+            <Text>
+              FilmLog&apos;s most popular film stock:{" "}
+              {allRolls.mostPopularStock}
+            </Text>
+            {user && <Text>Your most popular film stock: </Text>}
+          </Group>
+          <Group>
+            <Text>
+              FilmLog&apos;s most popular film format:{" "}
+              {allRolls.mostPopularFormat}
+            </Text>
+          </Group>
+          <Group>
+            <Text>
+              FilmLog&apos;s most popular film type: {allRolls.mostPopularType}
+            </Text>
+          </Group>
+        </Stack>
+      </Center>
+    </AppShellMain>
+  );
 }
